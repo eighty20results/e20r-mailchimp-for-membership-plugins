86ed7e5e4909c2424fc29dcc0b5a141a877d172d
Thomas Sjolshagen <thomas@eighty20results.com>
2018-11-26 15:40:19 -0700

    Update version number and description in plugin file

--- class.e20r-mailchimp-for-membership-plugins.php
+++ class.e20r-mailchimp-for-membership-plugins.php
@@ -1,12 +1,12 @@
 <?php
 /*
-Plugin Name: E20R MailChimp Interest Groups for Paid Memberships Pro and WooCommerce
+Plugin Name: E20R MailChimp Interest Groups for Paid Memberships Pro (and WooCommerce)
 Plugin URI: https://eighty20results.com/wordpress-plugins/e20r-mailchimp-for-membership-plugins/
-Description: Automatically add users to your MailChimp.com list(s) when they purchase, sign up, or register to get access your site/products. Segment users with Merge Tags and/or Mailchimp Interest Groups. Include custom user meta data in the merge tags/merge fields. Supports <a href="https://wordpress.org/plugins/paid-memberships-pro/">Paid Memberships Pro</a> and <a href="https://wordpress.org/plugins/woocommerce/">WooCommerce</a>
-Version: 2.11
+Description: Use MailChimp Interest Groups and Merge Fields when adding members to your MailChimp.com list(s) when they purchase, sign up, or register to get access your site/products. Segment users with Merge Tags and/or MailChimp Interest Groups. Include custom user meta data in the merge tags/merge fields. Supports <a href="https://wordpress.org/plugins/paid-memberships-pro/">Paid Memberships Pro</a> and <a href="https://wordpress.org/plugins/woocommerce/">WooCommerce</a>
+Version: 3.0
 WC requires at least: 3.3
 WC tested up to: 3.5
-Requires at least: 4.9
+Requires at least: 4.5
 Tested up to: 5.0
 Author: Eighty/20 Results <thomas@eighty20results.com>
 Author URI: https://eighty20results.com/thomas-sjolshagen/
@@ -49,7 +49,7 @@ if ( ! defined( 'E20R_MC_TESTING' ) ) {
 }
 
 if ( ! defined( 'E20R_MAILCHIMP_VERSION' ) ) {
-	define( 'E20R_MAILCHIMP_VERSION', '2.11' );
+	define( 'E20R_MAILCHIMP_VERSION', '3.0' );
 }
 
 if ( ! defined( 'E20R_MAILCHIMP_DIR' ) ) {
